6d0bf4cbaa98e15c7e1b0ebe39fd7cf610ccb123
thiepn <junsosmart@gmail.com>
2026-08-24 23:01:09 +0200

    R5: load desktop surface reconstruction layer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import "./styles.css";
 import "./interaction/r3.css";
 import "./workspace/r5Desktop.css";
+import "./workspace/r5DesktopSurfaces.css";
 import { applySettings, readSettings } from "./settings/settingsStore";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
 import { recordDiagnosticError } from "./diagnostics/errorRepository";
